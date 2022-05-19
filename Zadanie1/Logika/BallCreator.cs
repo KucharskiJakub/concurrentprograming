2e14dc95b4a734a874526ac90595d82e559fc24d
KucharskiJakub <236576@edu.p.lodz.pl>
2022-05-19 23:31:31 +0200

    zad 2

--- Zadanie1/Logika/BallCreator.cs
+++ Zadanie1/Logika/BallCreator.cs
@@ -76,6 +76,7 @@ namespace Logic
                 lock (locker)
                 {
                     LookForCollisionsNaive(balls, ball);
+
                 }
             }
         }
@@ -88,10 +89,62 @@ namespace Logic
         {
             // Iteruje po wszystkich parach kul. Jeśli są wystarczająco blisko siebie, to zatrzymuje ich ruch, oblicza 
             // prędkość po zderzeniu i wznawia ruch
+            foreach (Ball b in balls)
+            {
+                double z1, z2, r, r1, r2;
+                if (b == ball)
+                    continue;
+                r1 = b.R;
+                r2 = ball.R;
+                r1 = r1 / 2;
+                r2 = r2 / 2;
+                r = r1 + r2;
+                
+                double distance = Pythagoras(b.X, ball.X, b.Y, ball.Y, r1, r2);
+                z1 = Math.Abs(b.VX+r1 - ball.VX+r2);
+                z2 = Math.Abs(b.VY+r1 - ball.VY+r2);
+                if (z1+r >=distance && z2+r>=distance)
+                {
+                    if (b.VX<0) { b.X += (-z1 / 2) - r1; }
+                    else if (b.VX>=0) { b.X += (z1 / 2) - r1; }
+                    if (b.VY<0) { b.Y += (-z2 / 2) - r1; }
+                    else if (b.VY>=0) { b.Y += (z2 / 2) - r1; }
+                    if (ball.VX<0) { ball.X += (-z1 / 2) - r2; }
+                    else if (ball.VX>=0) { ball.X += (z1 / 2) - r2; }
+                    if (ball.VY<0) { ball.Y += (-z2 / 2) - r2; }
+                    else if (ball.VY>=0) { ball.Y += (z2 / 2) - r2; }
 
+                    
+                    rrr(ball, b);
+                }
+
+            }
         }
 
+        public async void rrr(Ball ball, Ball b)
+        {
+            await Task.Delay(20); 
+            b.VX = -b.VX;
+            b.VY = -b.VY;
+            ball.VX = -ball.VX;
+            ball.VY = -ball.VY;
+        }
 
+        public double Pythagoras(double x1, double x2, double y1, double y2, double r1, double r2)
+        {
+            double a, x, y;
+            x1 += r1;
+            y1 += r1;
+            x2 += r2;
+            y2 += r2;
+            x = x1 - x2;
+            y = y1 - y2;
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            a = x * x + y * y;
+            a = Math.Sqrt(a);
+            return a;
+        }
 
         public double CalculateVelocity(double v, double px, double py)
         {
